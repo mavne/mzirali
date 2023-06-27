b87e45e751a55ff6bbe80486a5e109beb04f7c73
giorgi <giorgigvazava87@gmail.com>
2023-06-27 17:09:29 +0400

    first commit

---
+++ _markup/index.php
@@ -0,0 +1,211 @@
+<?php 
+$version = "1.0.4";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" href="/_markup/assets/img/favicon.svg" type="image/svg" sizes="32x32">
+	<title>Mzirali</title>
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/bootstrap/css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/owl/owl.carousel.min.css" />
+	<link rel="stylesheet" href="/_markup/assets/lightbox/jquery.lightbox.css" />
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/css/main.css?v=<?=$version?>" />
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/aos/aos.css" />
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/css/ge.css?v=<?=$version?>" />
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/css/responsive_ge.css?v=<?=$version?>" />
+</head>
+<body>
+
+<div class="fixed-menu">
+	<div class="top">
+		<div class="center_small"></div>
+	</div>
+	<div class="bottom">
+		<div class="center_small"></div>
+	</div>
+</div>
+
+<div class="navigation-box">
+	<div class="center">
+		<a href="javascript:void(0)" class="close">close</a>
+
+		<div class="nav-box">
+			<nav>
+				<ul>
+					<li><a href="">შესახებ</a></li>
+					<li><a href="">ბინის არჩევა</a></li>
+					<li><a href="">ადგილმდებარეობა</a></li>
+					<li><a href="">საინვესტიციო მიმზიდველობა</a></li>
+					<li><a href="">კომპანია</a></li>
+					<li><a href="">კონტაქტი</a></li>
+					<li><a href="">სიახლეები</a></li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+</div>
+
+<div class="lines">
+	<div class="vertical">
+		<div class="left"></div>
+		<div class="middle"></div>
+		<div class="right"></div>
+	</div>
+
+	<div class="horizontal"></div>
+</div>
+
+<div class="logo-live-social">
+	<div class="center">
+		<a href="" class="logo"></a>
+		
+		<div class="right">
+			<div class="center_small">
+				<a href="" class="live-video">LIVE</a>
+			</div>
+
+			<ul class="social">
+				<li><a href="" class="facebook">facebook</a></li>
+				<li><a href="" class="instagram">instagram</a></li>
+				<li><a href="" class="telegram">telegram</a></li>
+				<li><a href="" class="whatsapp">whatsapp</a></li>
+				<li><a href="" class="viber">viber</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<header>
+	<div class="center">
+		<div class="center_small">
+			<div class="burger-language-data">
+				<a href="javascript:void(0)" class="burger">burger</a>
+
+				<div class="line"></div>
+
+				<ul class="languages">
+					<li><a href="" class="active">GE</a></li>
+					<li><a href="">EN</a></li>
+					<li><a href="">RU</a></li>
+				</ul>
+			
+				<div class="data-box">
+					<ul>
+						<li>
+							<h2>აპარტამენდები</h2>
+							<p>46 000-დან</p>
+						</li>
+
+						<li>
+							<h2>უკუგება</h2>
+							<p>5 - 6 წელი</p>
+						</li>
+
+						<li>
+							<h2>შემოსავალი</h2>
+							<p>10 000$ (წლიური)</p>
+						</li>
+					</ul>
+				</div>
+			</div>			
+
+			
+		</div>
+
+		<div class="contact-box">
+			<a href="" class="ask-call">ზარის შეკვეთა</a>
+			<a href="" class="call">+995 595 06 23 23</a>
+		</div>
+	</div>
+</header>
+
+<section class="image-text">
+	<div class="center_small multiplyColor height100">
+		<div class="mainColorBg"></div>
+		<div class="mainColorBg2"></div>
+		<div class="mainColorBg0"></div>
+		<div class="mainColorBg3"></div>
+		<div class="mainColorBg4"></div>
+	</div>
+
+	
+	<div class="text-box">
+		<div class="center_small height100">
+			<div class="box">
+				<div class="flexdisplay">
+					<h2>საცხოვრებელი და საინვესტიციო კომპლექსი (შავი) ზღვის სანაპიროზე</h2>
+					<p>ხარისხიანი ცხოვრებისთვის და დასვენებისთვის</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="image-box">
+		<div class="image" style="background-image: url('/_markup/assets/img/mainImage.jpg')"></div>
+	</div>
+</section>
+
+
+<main>
+	<section class="about">
+		<div class="center_small">
+			<h2>შესახებ</h2>
+
+			<div class="content">
+				<div class="outside-left">
+					<div class="image" style="background-image: url('/_markup/assets/img/left.jpg')"></div>
+				</div>
+				<div class="text">
+					<p>"მზირალი“ საცხოვრებელი და საინვესტიციო კომპლექსი შავი ზღვის სანაპიროზე, სადაც ყველა პირობაა შექმნილი მშვიდი, ხარისხიანი და კომფორტული ცხოვრებისთვის და დასვენებისთვის. აქაური გარემო ეკოლოგიურად სუფთა და ჯანსაღია, რასაც ზღვისა და მთის უნიკალური მიკროკლიმატი და უსუფთავესი ზღვის წყალი განაპირობებს. კომპლექსი ორი, 9 და 11 სართულიანი ბლოკისგან შედგება. თითოეულ სართულზე   მაქსიმუმ 5 ბინა განლაგდება. ყველა ბინა მზიანი და ნათელია. ბინებიდან იშლება ზღვისა და მთის ულამაზესი პანორამული ხედები, რაც აქ დასვენებას თუ ცხოვრებას ერთნაირად სასიამოვნოს ხდის. მშენებლობის პროცესში გამოყენებულია მხოლოდ ხარისხიანი სამშენებლო მასალები</p>
+
+					<ul class="data-list">
+						<li>
+							<div class="before"></div>
+							<em class="flat"></em>
+							<p>ბინების ჯამური რაოდენობა: 80</p>
+						</li>
+
+						<li>
+							<div class="before"></div>
+							<em class="calendar"></em>
+							<p>მშენებლობის დასრულება: 2024 წლის ბოლო </p>
+						</li>
+
+						<li>
+							<div class="before"></div>
+							<em class="guarantee"></em>
+							<p>3 წლიანი გარანტია რემონტზე და კარ-ფანჯრებზე </p>
+						</li>
+					</ul>
+				</div>
+
+				<div class="inside-image">
+					<div class="image" style="background-image: url('/_markup/assets/img/inside.jpg')"></div>
+				</div>
+				<div class="outside-right">
+					<div class="image" style="background-image: url('/_markup/assets/img/right.jpg')"></div>
+				</div>
+
+				<div class="clearer"></div>
+			</div>
+		</div>
+	</section>
+</main>
+
+<footer>
+
+</footer>
+
+
+
+<script type="text/javascript" src="/_markup/assets/js/jquery.js"></script>
+<script type="text/javascript" src="/_markup/assets/lightbox/jquery.lightbox.js"></script>
+<script type="text/javascript" src="/_markup/assets/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/_markup/assets/js/owl.carousel.min.js"></script>
+<script type="text/javascript" src="/_markup/assets/js/main.js?v=<?=$version?>"></script>
+<script type="text/javascript" src="/_markup/assets/aos/aos.js"></script>
+</body>
+
+</html>
---
+++ up.sh
@@ -0,0 +1,22 @@
+sass _markup/assets/scss/main.scss _markup/assets/css/main.css --style compressed;
+
+sass _markup/assets/scss/responsive_ge.scss _markup/assets/css/responsive_ge.css --style compressed;
+
+sass _markup/assets/scss/responsive_en.scss _markup/assets/css/responsive_en.css --style compressed;
+
+sass _markup/assets/scss/responsive_ru.scss _markup/assets/css/responsive_ru.css --style compressed;
+
+sass _markup/assets/scss/fonts_ge.scss _markup/assets/css/fonts_ge.css --style compressed;
+
+sass _markup/assets/scss/fonts_en.scss _markup/assets/css/fonts_en.css --style compressed;
+
+sass _markup/assets/scss/fonts_ru.scss _markup/assets/css/fonts_ru.css --style compressed;
+
+sass _markup/assets/scss/ge.scss _markup/assets/css/ge.css --style compressed;
+
+sass _markup/assets/scss/en.scss _markup/assets/css/en.css --style compressed;
+
+sass _markup/assets/scss/ru.scss _markup/assets/css/ru.css --style compressed;
+
+uglifyjs _markup/assets/js/main.js -o _markup/assets/js/compressed.js;
+
